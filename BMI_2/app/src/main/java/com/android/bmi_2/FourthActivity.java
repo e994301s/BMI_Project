8c0c8ff10af75aa2d76b9391131842b56f4bd29b
mg6613 <mg6613@naver.com>
2020-12-17 18:52:51 +0900

    yoo

--- BMI_2/app/src/main/java/com/android/bmi_2/FourthActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/FourthActivity.java
@@ -1,13 +1,69 @@
 package com.android.bmi_2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 
 public class FourthActivity extends Activity {
+
+    SeekBar sb01;
+    EditText editKg;
+    ImageView image1;
+    Button nextBtn;
+    int weight;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+
+        int size;
+        sb01 = findViewById(R.id.sb01);
+        editKg = findViewById(R.id.editKg);
+        image1 = findViewById(R.id.image1);
+        nextBtn = findViewById(R.id.next);
+
+        Intent intent = getIntent();
+        String female = intent.getStringExtra("female");
+        String male = intent.getStringExtra("male");
+        int height = intent.getIntExtra("height", 0);
+
+
+        sb01.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                editKg.setText("" + progress);
+                weight =progress;
+
+                if (progress >=150&&progress<160){
+                    image1.setImageResource(R.drawable.m1);
+                }else if(progress>=160&&progress<170){
+                    image1.setImageResource(R.drawable.m2);
+                }else if(progress>=170&&progress<180){
+                    image1.setImageResource(R.drawable.m3);
+                }else if (progress>=180&&progress<300){
+                    image1.setImageResource(R.drawable.m4);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
--- BMI_2/app/src/main/java/com/android/bmi_2/ThirdActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/ThirdActivity.java
@@ -1,15 +1,90 @@
 package com.android.bmi_2;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class ThirdActivity extends Activity {
+    final static int RValue = 0;
+    SeekBar sb01;
+    EditText editCm;
+    ImageView image1;
+    Button nextBtn;
+    int height;
+
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+
+        sb01 = findViewById(R.id.sb01);
+        editCm = findViewById(R.id.editCm);
+        image1 = findViewById(R.id.image1);
+        nextBtn = findViewById(R.id.next);
+
+        Intent intent = getIntent();
+        String female = intent.getStringExtra("female");
+        String male = intent.getStringExtra("male");
+
+        nextBtn.setOnClickListener(mClickListener);
+
+        sb01.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                editCm.setText("" + progress);
+                height =progress;
+
+                image1.setScaleType(ImageView.ScaleType.CENTER);
+
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
     }
+
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()) {
+
+                case R.id.next:
+                    intent = new Intent(ThirdActivity.this, FourthActivity.class);
+                    intent.putExtra("height", height);
+                    startActivity(intent);
+                    break;
+//                case  R.id.back:
+//                    intent = new Intent(MainActivity.this, SubActivity.class);
+//                    startActivity(intent);
+//                    break;
+                default:
+            }
+        }
+    };
 }
