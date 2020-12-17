a738e4c73b95378c12ba0e6047b430ab19e0f8f7
kyeongmipak <qkrrudal7@naver.com>
2020-12-18 00:06:27 +0900

    result page_kyeongmi

--- BMI_2/app/src/main/java/com/android/bmi_2/SeventhActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/SeventhActivity.java
@@ -1,5 +1,6 @@
 package com.android.bmi_2;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +10,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 public class SeventhActivity extends Activity {
 
-    TextView resultBMI, resultRecommend;
+    TextView resultBMI, resultRecommend, resultRecommend1;
     ImageView resultImage;
     int height, weight, weightGoal;
     double bmiResultCalc;
@@ -28,18 +30,29 @@ public class SeventhActivity extends Activity {
         weight = intent.getIntExtra("weight", 0);
 
         TextView current = findViewById(R.id.weightCurrent_seventh);
-        current.setText(Integer.toString(weight));
+        current.setText(Integer.toString(weight) + "kg");
 
         TextView goal = findViewById(R.id.weightGoal_seventh);
-        goal.setText(Integer.toString(weightCalc()));
+        goal.setText(Integer.toString(weightCalc())+ "kg");
 
         resultImage = findViewById(R.id.resultImage_seventh);
         resultBMI = findViewById(R.id.resultBMI_seventh);
         resultRecommend = findViewById(R.id.resultRecommend_seventh);
+        resultRecommend1 = findViewById(R.id.resultRecommend1_seventh);
 
-        resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
-        resultRecommend.setText(bmiType());
+        TextView lowerMax = findViewById(R.id.lowerMax_seventh);
+        lowerMax.setText(" < "+ lowerMaxCalc() + "kg");
 
+        TextView normalMin = findViewById(R.id.normalMin_seventh);
+        normalMin.setText(lowerMaxCalc() + "kg < ");
+
+        TextView normalMax = findViewById(R.id.normalMax_seventh);
+        normalMax.setText(" < "+ normalMaxCalc() + "kg");
+
+        TextView fatMin = findViewById(R.id.fatMin_seventh);
+        fatMin.setText(normalMaxCalc() + "kg < ");
+
+        bmiType();
         findViewById(R.id.btnExercise_seventh).setOnClickListener(mClick);
     }
 
@@ -60,25 +73,63 @@ public class SeventhActivity extends Activity {
         return weightGoal;
     }
 
+    // 저체중 체중 Max 계산 method (bmi 18.6 기준)
+    private String lowerMaxCalc(){
+
+        int weightMax = (int)(18.6 * height * height * 0.0001);
+        String max = Integer.toString(weightMax);
+        return max;
+    }
+
+    // 정상 체중 Max 계산 method (bmi 22.9 기준)
+    private String normalMaxCalc(){
+
+        int weightMax = (int)(22.9 * height * height * 0.0001);
+        String max = Integer.toString(weightMax);
+        return max;
+    }
+
     // bmi 결과에 따른 타입
-    private String bmiType(){
+    @SuppressLint("SetTextI18n")
+    private void bmiType(){
         bmiResultCalc = bmiCalc();
-        String type ="";
+        //String type ="";
 
         if(bmiResultCalc<18.6){
+           // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.sky));
+            resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
             resultImage.setImageResource(R.drawable.lower);
-            type = "저체중";
+            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.sky));
+            resultRecommend.setText("저체중");
+            resultRecommend1.setText("당신은 근육량을 " + (weightCalc()-weight) + "kg 증량을 추천합니다!");
+
         } else if (bmiResultCalc>=18.6 && bmiResultCalc<22.9){
+           // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.green));
+            resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
             resultImage.setImageResource(R.drawable.normal);
-            type = "정상";
+            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.green));
+            resultRecommend.setText("정상");
+            resultRecommend1.setText("당신은 꾸준한 운동으로 현재 몸을 유지하세요!");
+
+
         } else if (bmiResultCalc>=22.9 && bmiResultCalc<24.9) {
+           // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red));
+            resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
             resultImage.setImageResource(R.drawable.over);
-            type = "과체중";
+            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.red));
+            resultRecommend.setText("과체중");
+            resultRecommend1.setText("당신은 체중을 " + (weight - weightCalc()) + "kg 감량을 위해\n운동이 필요합니다!");
+
+
         } else if (bmiResultCalc>=24.9){
+           // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red));
+            resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
             resultImage.setImageResource(R.drawable.fat);
-            type = "비만";
+            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.red));
+            resultRecommend.setText("비만");
+            resultRecommend1.setText("당신은 체중을 " + (weight - weightCalc()) + "kg 감량을 위해\n운동과 식이요법을 병행하셔야 합니다!");
+
         }
-        return type;
     }
 
 
