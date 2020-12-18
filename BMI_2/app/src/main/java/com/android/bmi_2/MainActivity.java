602fe6368a2d5b51bbd0860d4e7ebc6cf8e7b4a8
young <775973@daum.net>
2020-12-18 10:14:06 +0900

    1218 main

--- BMI_2/app/src/main/java/com/android/bmi_2/MainActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/MainActivity.java
@@ -1,14 +1,61 @@
 package com.android.bmi_2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.BaseRequestOptions;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends Activity {
+        ImageView imageView;
+        Button button;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+            imageView = findViewById(R.id.mainimg);
+            Glide.with(this).load(R.raw.main).apply(new BaseRequestOptions() {
+                @Override
+                public BaseRequestOptions centerCrop() {
+                    return super.centerCrop();
+                }
+            }.circleCrop()).into(imageView);
+
+
+            findViewById(R.id.bmidetail).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final LinearLayout linear = (LinearLayout) View.inflate(com.android.bmi_2.MainActivity.this, R.layout.bmidetail, null);
+                    new AlertDialog.Builder(com.android.bmi_2.MainActivity.this)
+                            .setView(linear)
+                            .setPositiveButton("돌아가기", null)
+                            .show();
+                }
+
+            });
+
+      button = findViewById(R.id.enter_main);
+      button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+              startActivity(intent);
+          }
+      });
+
+        }
     }
-}
