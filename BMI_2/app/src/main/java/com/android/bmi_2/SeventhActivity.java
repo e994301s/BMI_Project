8573473f980565c2823938d3f414ea68f960868f
DESKTOP-RPFTG8R\admin <e994301s@gmail.com>
2020-12-19 13:01:32 +0900

    DB ing

--- BMI_2/app/src/main/java/com/android/bmi_2/SeventhActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/SeventhActivity.java
@@ -3,11 +3,14 @@ package com.android.bmi_2;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.NoCopySpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
@@ -18,11 +21,13 @@ public class SeventhActivity extends Activity {
 
     TextView resultBMI, resultRecommend, resultRecommend1;
     ImageView resultImage;
-    int height, weight, weightGoal;
+    int height, weight, weightGoal, userbmi;
     double bmiResultCalc;
+    private SQLiteDatabase DB;
+    HistoryDB historyDB;
 
-
-
+    String username;
+    private final String TAG = "Seventh";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,7 +38,7 @@ public class SeventhActivity extends Activity {
 
         height = intent.getIntExtra("height", 0);
         weight = intent.getIntExtra("weight", 0);
-
+        username = intent.getStringExtra("username");
 //        TextView current = findViewById(R.id.weightCurrent_seventh);
 //        current.setText(Integer.toString(weight) + "kg");
 //
@@ -59,6 +64,9 @@ public class SeventhActivity extends Activity {
 
         bmiType();
         findViewById(R.id.btnExercise_seventh).setOnClickListener(mClick);
+        weightGoal = weightCalc() - weight;
+        userbmi = (int) bmiCalc();
+        inserDB(height, weight, userbmi, weightGoal, bmiType(), username);
 
         resultRecommend = findViewById(R.id.history_seventh);
         resultRecommend1 = findViewById(R.id.evaluation_seventh);
@@ -116,8 +124,9 @@ public class SeventhActivity extends Activity {
 
     // bmi 결과에 따른 타입
     @SuppressLint("SetTextI18n")
-    private void bmiType(){
+    private int bmiType(){
         bmiResultCalc = bmiCalc();
+        int resulticon = 0;
         //String type ="";
 
         if(bmiResultCalc<18.6){
@@ -127,7 +136,7 @@ public class SeventhActivity extends Activity {
 //            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.sky));
 //            resultRecommend.setText("저체중");
 //            resultRecommend1.setText("당신은 근육량을 " + (weightCalc()-weight) + "kg 증량을 추천합니다!");
-
+            resulticon =  R.drawable.lower;
         } else if (bmiResultCalc>=18.6 && bmiResultCalc<22.9){
            // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.green));
             resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
@@ -135,7 +144,7 @@ public class SeventhActivity extends Activity {
 //            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.green));
 //            resultRecommend.setText("정상");
 //            resultRecommend1.setText("당신은 꾸준한 운동으로 현재 몸을 유지 하세요!");
-
+            resulticon = R.drawable.normal;
 
         } else if (bmiResultCalc>=22.9 && bmiResultCalc<24.9) {
            // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red));
@@ -145,16 +154,17 @@ public class SeventhActivity extends Activity {
 //            resultRecommend.setText("과체중");
 //            resultRecommend1.setText("당신은 체중을 " + (weight - weightCalc()) + "kg 감량을 위해\n운동이 필요합니다!");
 
-
+            resulticon = R.drawable.over;
         } else if (bmiResultCalc>=24.9){
            // resultBMI.setTextColor(ContextCompat.getColor(this, R.color.red));
             resultBMI.setText(Double.toString(bmiCalc()).substring(0, 5));
             resultImage.setImageResource(R.drawable.fat);
 //            resultRecommend.setTextColor(ContextCompat.getColor(this, R.color.red));
 //            resultRecommend.setText("비만");
-//            resultRecommend1.setText("당신은 체중을 " + (weight - weightCalc()) + "kg 감량을 위해\n운동과 식이요법을 병행하셔야 합니다!");
-
+//            resultRecommend1.setText("-1당신은 체중을 " + (weight - weightCalc()) + "kg 감량을 위해\n운동과 식이요법을 병행하셔야 합니다!");
+            resulticon = R.drawable.fat;
         }
+        return resulticon;
     }
 
 
@@ -172,4 +182,24 @@ public class SeventhActivity extends Activity {
             startActivity(intent);
         }
     };
+
+    private void inserDB(int dbheight, int dbweight, int dbbmi, int dbweightGoal, int dbicon, String dbusername){
+        historyDB = new HistoryDB(SeventhActivity.this);
+        Log.v(TAG, Integer.toString(dbheight));
+        Log.v(TAG, Integer.toString(dbweight));
+        Log.v(TAG, Integer.toString(dbbmi));
+        Log.v(TAG, Integer.toString(dbweightGoal));
+        Log.v(TAG, Integer.toString(dbicon));
+
+        try {
+            DB = historyDB.getWritableDatabase();
+            String query = "INSERT INTO user (username, userheight, userweight, userbmi, userneed, usericon) VALUES ('"+dbusername+"',"+dbheight+", "+dbweight+", "+dbbmi+", "+dbweightGoal+", "+dbicon+");";
+            Toast.makeText(SeventhActivity.this,"입력 완료", Toast.LENGTH_SHORT).show();
+            DB.execSQL(query);
+            historyDB.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(SeventhActivity.this,"입력 실패", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
